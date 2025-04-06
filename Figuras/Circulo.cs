a7d44236523457cd706c652b196e48cf5e483c2b
pablocabe <pablocabe0@gmail.com>
2025-04-06 12:38:23 -0300

    .

--- Figuras/Circulo.cs
+++ Figuras/Circulo.cs
@@ -13,21 +13,3 @@ public class Circulo
         return area;
     }
 }
-
-public class Rectangulo
-{
-    private double _base;
-    private double _altura;
-
-    public Rectangulo (double _base, double _altura){
-        this._base = _base;
-        this._altura = _altura;
-    }
-
-    public double GetArea(){
-        double area = _base * _altura;
-        return area;
-    }
-}
-
-
--- Figuras/Rectangulo.cs
+++ Figuras/Rectangulo.cs
@@ -4,5 +4,16 @@ namespace Figuras;
 
 public class Rectangulo
 {
+    private double _base;
+    private double _altura;
 
+    public Rectangulo (double _base, double _altura){
+        this._base = _base;
+        this._altura = _altura;
+    }
+
+    public double GetArea(){
+        double area = _base * _altura;
+        return area;
+    }
 }
